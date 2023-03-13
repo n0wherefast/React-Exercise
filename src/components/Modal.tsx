55e7b803344597e6be51506f76ca4bd3c2d1723d
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2023-03-13 01:50:32 +0100

    UI fixed

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -17,9 +17,9 @@ export default function Modal() {
                 <h1 className=' text-3xl font-bold text-pink-600 p-4 '>Modal + Auto Complete</h1>
 
         <Button className=' p-2 rounded-xl border-2  bg-cyan-600 hover:bg-pink-700 hover:border-2 border-cyan-600 max-w-1/2 m-2 font-semibold' type='submit' onClick={create} name={'Search Bar'}/>
-        <div className ={`flex justify-center bg-slate-800 w-[300px] pl-2 pr-2 rounded-xl ${openModal}`}>
+        <div className ={`flex justify-cemter bg-slate-800 w-[310px] p-3 rounded-xl ${openModal}`}>
              <AutoComplete/>
-             <Button className='border border-slate-900 p-3  rounded  bg-pink-700 hover:bg-pink-800  max-h-6 flex items-center m-3 mr-5' type='submit' onClick={canc} name={'X'}/>
+             <Button className='border border-slate-900 p-3 rounded  bg-pink-700 hover:bg-pink-800  max-h-6 flex items-center m-3 mr-' type='submit' onClick={canc} name={'X'}/>
         </div>
     </div>
   )
